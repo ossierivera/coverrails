e864be167d86658add58b51fd3f924f9f54d093c
Ossie Rivera <os.rivera@gmail.com>
2019-05-16 22:43:35 -0400

    Tests - Mandatory integration test, added controller test, model test of encrypted_string and rotate_key

--- test/models/encrypted_string_test.rb
+++ test/models/encrypted_string_test.rb
@@ -2,7 +2,27 @@ require 'test_helper'
 
 class EncryptedStringTest < ActiveSupport::TestCase
 
-  test 'Missing tests! Please fix' do
+  test "reencrypt!" do
+  	# Create an encrypted string
+    encrypted_string = EncryptedString.create!(value: "Test string")
+    
+    # Generate a new primary key
+    DataEncryptingKey.generate_new_primary
+    new_key = DataEncryptingKey.primary
+    # reencrypt with the new primary key
+    encrypted_string.reencrypt!(new_key)
 
+    # make sure the data encrypting key is the new one
+    assert_equal new_key.id, encrypted_string.data_encrypting_key.id
+    # make sure the original string can be retrieved.
+    assert_equal "Test string", encrypted_string.value
+  end
+
+  test "create" do
+  	assert_difference "EncryptedString.count" do
+      encrypted_string = EncryptedString.create!(value: "Test string")
+      assert encrypted_string.token
+      assert encrypted_string.data_encrypting_key
+    end
   end
 end
